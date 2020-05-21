b147c37f87fba2844bb85fdb7beeaf8cefa37536
elliottloxley <elliottloxley@gmail.com>
2020-05-21 14:49:52 +0100

    start of web development

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <MainPageTitle title="Elliott Chisholm Loxley"/>
+  </div>
+</template>
+
+<script>
+import MainPageTitle from './components/MainPageTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainPageTitle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
